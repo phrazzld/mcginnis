1618a1c038575ceb1fce93f84883e1c7d1e99ef4
Phaedrus Raznikov <praznikov@cerego.com>
2020-02-10 12:48:52 -0800

    Init to winit with React

---
+++ react/github-battle/app/components/Battle.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  FaUserFriends,
+  FaFighterJet,
+  FaTrophy,
+  FaTimesCircle,
+} from 'react-icons/fa';
+
+function Instructions() {
+  return (
+    <div className="instructions-container">
+      <h1 className="center-text header-lg">Instructions</h1>
+      <ol className="container-sm grid center-text battle-instructions">
+        <li>
+          <h3 className="header-sm">Enter two Github users</h3>
+          <FaUserFriends
+            className="bg-light"
+            color="rgb(255, 191, 116)"
+            size={140}
+          />
+        </li>
+        <li>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className="bg-light" color="#727272" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">Enter two Github users</h3>
+          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140} />
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <form className="column player" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+        <div className="row player-inputs">
+          <input
+            type="text"
+            id="username"
+            className="input-light"
+            placeholder="github username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className="btn dark-btn"
+            type="submit"
+            disabled={!this.state.username}>
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+function PlayerPreview({username, onReset, label}) {
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className="row bg-light">
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player,
+    });
+  }
+
+  handleReset(id) {
+    this.setState({
+      [id]: null,
+    });
+  }
+
+  render() {
+    const {playerOne, playerTwo} = this.state;
+
+    return (
+      <React.Fragment>
+        <Instructions />
+
+        <div className="players-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-around">
+            {playerOne === null ? (
+              <PlayerInput
+                label="Player One"
+                onSubmit={player => this.handleSubmit('playerOne', player)}
+              />
+            ) : (
+              <PlayerPreview
+                username={playerOne}
+                label="Player One"
+                onReset={() => this.handleReset('playerOne')}
+              />
+            )}
+            {playerTwo === null ? (
+              <PlayerInput
+                label="Player Two"
+                onSubmit={player => this.handleSubmit('playerTwo', player)}
+              />
+            ) : (
+              <PlayerPreview
+                username={playerTwo}
+                label="Player Two"
+                onReset={() => this.handleReset('playerTwo')}
+              />
+            )}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
---
+++ react/github-battle/app/components/Popular.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {fetchPopularRepos} from '../utils/api';
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle,
+} from 'react-icons/fa';
+
+function LanguagesNav({selected, onUpdateLanguage}) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="flex-center">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? {color: 'rgb(187, 46, 31)'} : null}
+            onClick={() => onUpdateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
+
+function ReposGrid({repos}) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues,
+        } = repo;
+        const {login, avatar_url} = owner;
+
+        return (
+          <li key={html_url} className="repo bg-light">
+            <h4 className="header-lg center-text">#{index + 1}</h4>
+            <img
+              className="avatar"
+              src={avatar_url}
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className="center-text">
+              <a className="link" href={html_url}>
+                {login}
+              </a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color="rgb(255, 191, 116)" size={22} />
+                <a href={`https://github.com/${login}`}>{login}</a>
+              </li>
+              <li>
+                <FaStar color="rgb(255, 215, 0)" size={22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color="rgb(129, 195, 0)" size={22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                {open_issues.toLocaleString()} open issues
+              </li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+      repos: {},
+      error: null,
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+    this.isloading = this.isLoading.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+      error: null,
+    });
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then(data => {
+          this.setState(({repos}) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data,
+            },
+          }));
+        })
+        .catch(() => {
+          console.warn(`Error fetching repos: ${error}`);
+
+          this.setState({
+            error: `There was an error fetching the repositories.`,
+          });
+        });
+    }
+  }
+
+  isLoading() {
+    const {selectedLanguage, repos, error} = this.state;
+
+    return !repos[selectedLanguage] && error === null;
+  }
+
+  render() {
+    const {selectedLanguage, repos, error} = this.state;
+
+    return (
+      <React.Fragment>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]} />
+        )}
+      </React.Fragment>
+    );
+  }
+}
